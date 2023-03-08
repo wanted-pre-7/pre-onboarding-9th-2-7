352e59cfd4ad11e7a381f6e629e6a71977364af1
sol <osunpine@gmail.com>
2023-03-09 07:23:04 +0900

    feat[#5]: 상품 리스트 필터링 구현

---
+++ src/components/Filter.tsx
@@ -0,0 +1,119 @@
+import {
+  Box,
+  Checkbox,
+  Container,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  RangeSliderTrack,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import type { Dispatch, SetStateAction } from "react";
+
+type PropsType = {
+  minPrice: number;
+  maxPrice: number;
+  spaces: string[];
+  setPrice: Dispatch<SetStateAction<number[]>>;
+  setSpaces: Dispatch<SetStateAction<string[]>>;
+};
+
+const Filter = ({
+  minPrice,
+  maxPrice,
+  spaces,
+  setPrice,
+  setSpaces,
+}: PropsType) => {
+  useEffect(() => {
+    let idArr: string[] = [];
+    spaceList.map((el) => idArr.push(el));
+    setSpaces(idArr);
+  }, []);
+
+  const handleAllCheck = (checked: boolean) => {
+    if (checked) {
+      let idArr: string[] = [];
+      spaceList.map((el) => idArr.push(el));
+      setSpaces(idArr);
+    } else setSpaces([]);
+  };
+
+  const handleSingleCheck = (checked: boolean, space: string) => {
+    if (checked) setSpaces([...spaces, space]);
+    else setSpaces(spaces.filter((el) => el !== space));
+  };
+
+  return (
+    <Box
+      maxW="600px"
+      display="flex"
+      flexDir="column"
+      p="15px"
+      bg="gray.50"
+      border="1px"
+      borderColor="gray.100"
+      borderRadius="4px"
+      m={["0px", "auto"]}
+    >
+      <Container>
+        <Text fontSize="15px" as="b">
+          가격 <br />
+          <Box as="span" fontSize="13px" fontWeight="normal">
+            ({minPrice.toLocaleString("ko-kr")}원 ~{" "}
+            {maxPrice.toLocaleString("ko-kr")}원)
+          </Box>
+        </Text>
+        <Container mt="10px">
+          <RangeSlider
+            onChangeEnd={([min, max]) => setPrice([min, max])}
+            colorScheme="cyan"
+            defaultValue={[0, 30000]}
+            min={0}
+            max={30000}
+            step={5000}
+          >
+            <RangeSliderTrack>
+              <RangeSliderFilledTrack />
+            </RangeSliderTrack>
+            <RangeSliderThumb boxSize={6} index={0} />
+            <RangeSliderThumb boxSize={6} index={1} />
+          </RangeSlider>
+        </Container>
+      </Container>
+      <Container mt="10px">
+        <Text fontSize="15px" as="b">
+          지역
+        </Text>
+        <Box mt="10px" display="flex" justifyContent="space-around">
+          <Checkbox
+            size="sm"
+            colorScheme="cyan"
+            value="전체"
+            isChecked={spaces.length === spaceList.length}
+            onChange={(e) => handleAllCheck(e.target.checked)}
+          >
+            전체
+          </Checkbox>
+          {spaceList.map((space) => (
+            <Checkbox
+              key={space}
+              size="sm"
+              colorScheme="cyan"
+              value={space}
+              isChecked={!!spaces.includes(space)}
+              onChange={(e) => handleSingleCheck(e.target.checked, space)}
+            >
+              {space}
+            </Checkbox>
+          ))}
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+const spaceList = ["강원", "대구", "부산", "서울", "제주"];
+
+export default Filter;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,20 @@
 import { Box, Container, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import Filter from "../components/Filter";
 import Product from "../components/Product";
 import useProductsQuery from "../query/useProductsQuery";
 
 const MainPage = () => {
   const { data, isFetching, isLoading } = useProductsQuery();
+  const [spaces, setSpaces] = useState<string[]>([]);
+  const [[minPrice, maxPrice], setPrice] = useState<number[]>([0, 30000]);
+
+  const products = data?.data.filter(
+    (item) =>
+      [...spaces].includes(item.spaceCategory) &&
+      item.price >= minPrice &&
+      item.price <= maxPrice,
+  );
 
   if (isFetching || isLoading) {
     return (
@@ -21,13 +32,20 @@ const MainPage = () => {
   }
   return (
     <Box w="80%" m="auto" p="30px" color="gray.800">
+      <Filter
+        minPrice={minPrice}
+        maxPrice={maxPrice}
+        setPrice={setPrice}
+        spaces={spaces}
+        setSpaces={setSpaces}
+      />
       <Box mt="20px">
         <Text fontSize="20px" as="b">
-          상품 ({data?.data.length})
+          상품 ({products?.length})
         </Text>
-        {data?.data.length ? (
+        {products?.length ? (
           <SimpleGrid w="100%" minChildWidth="200px" spacing="20px" mt="10px">
-            {data?.data.map((item) => (
+            {products?.map((item) => (
               <Product key={item.idx} info={item} />
             ))}
           </SimpleGrid>
