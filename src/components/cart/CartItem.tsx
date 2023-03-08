a87688f2ea14c920d13973c58a3b437221c43752
che-97 <rue015@gmail.com>
2023-03-09 00:19:54 +0900

    feat[#6]: 장바구니 리스트 구현
    - [#6] 리스트 보여주고 삭제
    - [#7] 구매 수량 변경
    - [#8] 총 결제액 수 표시

---
+++ src/components/cart/CartItem.tsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { useAppDispatch } from "../../app/hook";
+import type { ICartItem } from "../../features/cartSlice";
+import { cartActions } from "../../features/cartSlice";
+
+const CartItem = ({ cartItem }: { cartItem: ICartItem }) => {
+  const toast = useToast();
+  const dispatch = useAppDispatch();
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(cartItem.idx));
+  };
+  const removeHandler = () => {
+    dispatch(cartActions.removeFromCart(cartItem.idx));
+  };
+
+  const addItemHandler = () => {
+    if (cartItem.maximumPurchases == cartItem.quantity) {
+      toast({
+        title: "최대 구매 수량을 초과하였습니다.",
+        status: "error",
+        isClosable: true,
+        position: "bottom-right",
+      });
+    } else {
+      dispatch(
+        cartActions.addItemToCart({
+          idx: cartItem.idx,
+          price: cartItem.price,
+          name: cartItem.name,
+          mainImage: cartItem.mainImage,
+          description: cartItem.description,
+          spaceCategory: cartItem.spaceCategory,
+          maximumPurchases: cartItem.maximumPurchases,
+          registrationDate: cartItem.registrationDate,
+        }),
+      );
+    }
+  };
+  return (
+    <Card
+      direction={{ base: "column", sm: "row" }}
+      overflow="hidden"
+      variant="outline"
+    >
+      <Image
+        objectFit="cover"
+        maxW={{ base: "100%", sm: "200px" }}
+        src={cartItem.mainImage}
+        alt="Caffe Latte"
+      />
+      <Stack>
+        <CardBody>
+          <Heading size="md">{cartItem.name}</Heading>
+
+          <Text py="2">{cartItem.description}</Text>
+        </CardBody>
+
+        <CardFooter>
+          <Text mr="5" fontSize={"xl"} color="blue.600">
+            {cartItem.totalPrice} 원
+          </Text>
+
+          <Button mr="5" onClick={addItemHandler}>
+            +
+          </Button>
+          <span>{cartItem.quantity}</span>
+          <Button ml="5" onClick={removeItemHandler}>
+            -
+          </Button>
+          <Button
+            onClick={removeHandler}
+            ml="5"
+            variant="solid"
+            colorScheme="red"
+          >
+            삭제
+          </Button>
+        </CardFooter>
+      </Stack>
+    </Card>
+  );
+};
+
+export default CartItem;
--- src/routes/Reservations.tsx
+++ src/routes/Reservations.tsx
@@ -1,5 +1,34 @@
+import { Box, Center, Heading, Text, VStack } from "@chakra-ui/react";
+import { useAppSelector } from "../app/hook";
+import CartItem from "../components/cart/CartItem";
+
 const Reservations = () => {
-  return <div></div>;
+  const cart = useAppSelector((state) => state.cart);
+
+  return (
+    <Center>
+      <Box w="70%" mt="10">
+        <Heading mb="5">장바구니</Heading>
+        {cart.totalQuantity == 0 ? (
+          <Box minH="200" backgroundColor={"gray.100"}>
+            <Center>
+              <Text mt="75">장바구니가 비어있습니다.</Text>
+            </Center>
+          </Box>
+        ) : (
+          <Box minH="200" borderColor="black">
+            {cart.items.map((item) => (
+              <CartItem key={item.idx} cartItem={item} />
+            ))}
+          </Box>
+        )}
+
+        <Text mt="5" align="right" fontSize={"2xl"} color="blue.600">
+          Total : {cart.totalPrice} 원
+        </Text>
+      </Box>
+    </Center>
+  );
 };
 
 export default Reservations;
