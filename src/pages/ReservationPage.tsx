d6ec7b51355bba9717bf840d124c550da20deafa
ilgon0110 <moses011010@gmail.com>
2023-03-09 03:11:10 +0900

    feat[#8]: 장바구니 총 결제액 수 기능 구현
    Redux-toolkit을 사용하여 기능 구현

--- src/pages/ReservationPage.tsx
+++ src/pages/ReservationPage.tsx
@@ -4,10 +4,16 @@ import Reservation from '../components/Reservation';
 import type { IState } from '../stores';
 
 const ReservationPage = () => {
-  const { reservationList } = useSelector((state: IState) => state);
-
+  const { reservationList, purchaseList } = useSelector(
+    (state: IState) => state,
+  );
+  const total = purchaseList.reduce(
+    (acc, cur) => acc + cur.count * cur.price,
+    0,
+  );
   return (
     <>
+      <h1>{total}</h1>
       {reservationList.map((product: IProduct) => {
         return <Reservation key={product.idx} product={product} />;
       })}
