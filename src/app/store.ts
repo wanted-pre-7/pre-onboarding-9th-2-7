62e5ff480dd533d027f20d0608ffdedad4c642f2
yujiseok <koesijuy@gmail.com>
2023-03-09 23:52:23 +0900

    feat[#18]: 세션스토리지를 이용해 새로고침시 상태 유지
    - middleware 활용

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cartSlice";
+import { sessionStorageMiddleware } from "./middleware";
+
+const reHydrateStore = () => {
+  if (sessionStorage.getItem("cart") !== null) {
+    return JSON.parse(sessionStorage.getItem("cart") as string);
+  }
+};
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
+  preloadedState: reHydrateStore(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sessionStorageMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
