a9921b69b0fa696d7378ff8b686f1e3ddc88cd28
violetwhenisawu <anashape710@gmail.com>
2023-03-09 21:36:24 +0900

    feat[#26]: 헤더 UI 추가, 장바구니 아이콘에 담긴 수량 표시하도록 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Header from "./components/common/Header";
 import Cart from "./pages/Cart";
 import Main from "./pages/Main";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/main" element={<Main />}></Route>
         <Route path="/reservations" element={<Cart />}></Route>
