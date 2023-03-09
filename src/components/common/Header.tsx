47f14ecfb7164b8ca2a4cf8b954808fde6915b5e
che-97 <rue015@gmail.com>
2023-03-09 22:53:17 +0900

    style[#26] 장바구니 아이콘 및 로고 수정 (원호님 참고)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,11 @@
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
+import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import HeaderCartButton from "./HeaderCartButton";
+
+const Logo = styled.img`
+  width: 150px;
+`;
 
 const Header = () => {
   return (
@@ -12,7 +17,7 @@ const Header = () => {
     >
       <Box>
         <Link to={"/main"}>
-          <Heading color={"blue"}>Like a local</Heading>
+          <Logo src="src/assets/logo.png" alt="logo" />
         </Link>
       </Box>
       <HStack spacing={2}>
