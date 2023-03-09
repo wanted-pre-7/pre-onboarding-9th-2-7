b391ca550edd42aefe336dd5d2b0638486611867
yujiseok <koesijuy@gmail.com>
2023-03-09 23:58:15 +0900

    refactor: 타입 수정

--- src/utils/getTotals.ts
+++ src/utils/getTotals.ts
@@ -1,11 +1,11 @@
-import type { IProduct } from "../types/product";
+import type { ICart } from "../types/product";
 
-export const getTotal = (cart: IProduct[]) => {
+export const getTotal = (cart: ICart[]) => {
   let totalQuantity = 0;
   let totalPrice = 0;
   cart.forEach((item) => {
-    totalQuantity += item.quantity!;
-    totalPrice += item.price * item.quantity!;
+    totalQuantity += item.quantity;
+    totalPrice += item.price * item.quantity;
   });
 
   return { totalQuantity, totalPrice };
