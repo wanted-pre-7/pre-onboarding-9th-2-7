945eaecd98ccef7b15940aa348ecd8e5dadfa413
violetwhenisawu <anashape710@gmail.com>
2023-03-10 08:37:14 +0900

    style: chakra-ui/icons 추가, 레이아웃 수정

--- src/components/Reservations/Layout.tsx
+++ src/components/Reservations/Layout.tsx
@@ -6,7 +6,7 @@ interface IChildren {
 
 const Layout = ({ children }: IChildren) => {
   return (
-    <Container mt="120px" maxW="700px" padding={{ base: 10, lg: 10 }}>
+    <Container mt="120px" maxW="800px" padding={{ base: 10, lg: 10 }}>
       <SimpleGrid columns={1} spacing={{ sm: 3, lg: 1 }}>
         {children}
       </SimpleGrid>
