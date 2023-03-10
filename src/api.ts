be82efa6ed933d318f066c6138c2462a97908257
violetwhenisawu <anashape710@gmail.com>
2023-03-10 09:25:35 +0900

    feat: mock 데이터 public 폴더로 경로 변경

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `http://localhost:3000/products`;
+const BASE_URL = `http://localhost:3000/data/data.json`;
 
 export const fetchProducts = async () => {
   const response = await (await fetch(`${BASE_URL}`)).json();
